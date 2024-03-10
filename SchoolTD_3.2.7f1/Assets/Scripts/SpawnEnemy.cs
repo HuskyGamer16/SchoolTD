6b25122a531a9e26edaeaa36e2864f98a9562a57
HuskyGamer16 <zsirkiraly01@gmail.com>
2024-03-10 12:47:19 +0100

    SpawnEnemy.cs created
    SpawnEnemy.cs created, but not implemented, resulotion still unchanged

---
+++ SchoolTD_3.2.7f1/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void SpawnNewEnemy(GameObject Enemy)
+    {
+        
+    }
+    void Update()   
+    {
+
+    }
+}
