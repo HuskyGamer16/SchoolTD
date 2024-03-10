cd2c00c892f58bbdfb53e645c1cb458e425f09f1
HuskyGamer16 <zsirkiraly01@gmail.com>
2024-03-10 18:03:16 +0100

    Big Project Hours
    12:15-18:00;
    Finalized the Desk model, Started the actual work on the levels, run into some problems, but  intending to fix it later.
    Now onto phase 1.2;
    
    Co-Authored-By: Intenmin <159892352+Intenmin@users.noreply.github.com>

--- SchoolTD_3.2.7f1/Assets/Scripts/ResolutionChange.cs
+++ SchoolTD_3.2.7f1/Assets/Scripts/ResolutionChange.cs
@@ -1,23 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using TMPro;
+using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class ResolutionChange : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown resDropDown;
     private TextMeshProUGUI text;
     private TextMeshProUGUI res;
     private Resolution[] Resol;
     private List<Resolution> filteredRes;
-    private float currentRefRate;
-    private int currentResIndex = 0;
-    private string[] res_available = new string[]{"3840x2160","2560x1440","2048x1152","1920x1080","1366x768","1280x720","960x540","640x360"};
 
+    private RefreshRate currentRefRate;
+    private int currentResIndex = 0;
+    [SerializeField] private IPanel Panel; 
+    
     private void Start()
     {
         Resol = Screen.resolutions;
         filteredRes = new List<Resolution>();
+        resDropDown.ClearOptions();
+        currentRefRate = Screen.currentResolution.refreshRateRatio;
+        for (int i = 0; i < Resol.Length; i++)
+        {
+            if (Resol[i].refreshRateRatio.value == currentRefRate.value) {
+                filteredRes.Add(Resol[i]);
+            }
+        }
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < filteredRes.Count; i++)
+        {
+            string filteredOpt = filteredRes[i].width +"x"+  filteredRes[i].height + " "+filteredRes[i].refreshRateRatio.ToString() + " Hz";
+            options.Add(filteredOpt);
+            if (filteredRes[i].width == Screen.width && filteredRes[i].height == Screen.height) {
+                currentResIndex = i;
+            }
+        }
+
+        resDropDown.AddOptions(options);
+        resDropDown.value = currentResIndex;
+        resDropDown.RefreshShownValue();
+    }
+    private void Update()
+    {
+        //    if(Input.GetKey(KeyCode.Escape) && (bool)Panel.)
+        //    {
+        //        //Debug.Log("Escapse pressed");
+        //        Panel.SetActive(false);
+        //    }
+        //    if (Input.GetKey(KeyCode.Escape) && !(bool)Panel.activeSelf)
+        //    {
+        //        //Debug.Log("Escapse pressed");
+        //        Panel.SetActive(true);
+        //    }
     }
     public void IncRes()
     {
@@ -26,5 +66,9 @@ public class ResolutionChange : MonoBehaviour
     public void DecRes() 
     {
     
+    }
+    public void SetRes(int resIndex) { 
+        Resolution resolution = filteredRes[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
     }
 }
