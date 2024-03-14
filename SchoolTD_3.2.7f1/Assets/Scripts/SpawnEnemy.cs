869d09dd41e8ae227e0e6fa028c8157ebf4c0802
HuskyGamer16 <zsirkiraly01@gmail.com>
2024-03-14 12:41:46 +0100

    Prefab created, somewhat sqlized
    some prefabs needed and the sql server is somewhat operational, but the laptop is at its end so from now on i have to use my home pc

--- SchoolTD_3.2.7f1/Assets/Scripts/SpawnEnemy.cs
+++ SchoolTD_3.2.7f1/Assets/Scripts/SpawnEnemy.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
-
 public class SpawnEnemy : MonoBehaviour
 {
-    
-    // Update is called once per frame
-    void SpawnNewEnemy(GameObject Enemy)
-    {
-        
-    }
-    void Update()   
-    {
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
 
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(myPrefab, new Vector3(-840, 0, 275), Quaternion.identity);
     }
 }
