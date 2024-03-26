a1a3ef672fd04d5ab5f512b7e391d245a18bd41d
HuskyGamer16 <zsirkiraly01@gmail.com>
2024-03-26 13:50:15 +0100

    NavMesh fixed
    NavMeshAgent somewhat reworked,
    The NavMeshSurface is (i think done),
    now onto fixing the agents' rotation

--- SchoolTD_3.2.7f1/Assets/Scripts/EnemyMovement.cs
+++ SchoolTD_3.2.7f1/Assets/Scripts/EnemyMovement.cs
@@ -11,11 +11,13 @@ public class EnemyMovement : MonoBehaviour
 
     void Start(){
         agent = GetComponent<NavMeshAgent>();
+        EndPos = GameObject.Find("KeepAlive");
     }
     // Update is called once per frame
     void Update()
     {
         //gameObject.transform.position += speed * Time.deltaTime * Vector3.back;
+        agent.speed = speed;
         agent.destination = EndPos.transform.position;
     }
 }
